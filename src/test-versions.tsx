d78fac7a729d754d169f910fa4acc8a0a90e0730
Dan Velton <48307985+dvelton@users.noreply.github.com>
2025-06-23 22:00:49 +0000

    Generated by Spark: Fix all reported errors. Fix all the following errors: Uncaught Error: Incompatible React versions: The "react" and "react-dom" packages must have the exact same version. Instead got:   - react:      19.1.0   - react-dom:  19.0.0 Learn more: https://react.dev/warnings/version-mismatch

---
+++ src/test-versions.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Log versions to check they match
+console.log('React version:', React.version);
+console.log('ReactDOM version:', ReactDOM.version);
+
+const TestComponent = () => <div>Testing React versions</div>;
+
+export default TestComponent;
